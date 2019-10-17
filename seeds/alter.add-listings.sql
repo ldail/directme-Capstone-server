c34ab320dbf2a59aeeb5b0266b3e55b3ec01b04b
Leon Dail <leondailofficial@gmail.com>
2019-10-17 12:46:55 -0700

    added description to table and seeded descriptions

--- seeds/alter.add-listings.sql
+++ seeds/alter.add-listings.sql
@@ -1,24 +1,12 @@
 TRUNCATE TABLE IF EXISTS listings;
-TRUNCATE TABLE IF EXISTS tag_listings;
 
-INSERT INTO listings (name, url)
+INSERT INTO listings (name, url, description)
 VALUES
-('Official Documentation', 'http://reactjs.com'), 
-('W3Schools','http://w3schools.com'), 
-('Photoshop Tutorials', 'https://www.photoshopessentials.com/'), 
-('Javascript Official Site', 'https://javascript.com'), 
-('Addicting Games', 'http://addictinggames.com'), 
-('Board Game Geek', 'http://boardgamegeek.com'), 
+('Official Documentation', 'http://reactjs.com', 'A great site to learn all the official documentation on React. I highly recommend their walkthroughs'), 
+('W3Schools','http://w3schools.com', 'Okay site that really go the job done back in the day, but now theres a bit to be desired'), 
+('Photoshop Tutorials', 'https://www.photoshopessentials.com/', 'Photoshop tutorials'), 
+('Javascript Official Site', 'https://javascript.com', 'Wanna learn javascript?'), 
+('Addicting Games', 'http://addictinggames.com', 'Prepare to waste hours of your life here.'), 
+('Board Game Geek', 'http://boardgamegeek.com', 'Board game ratings, reviews, community.'), 
 ('Pogo Online Gaming', 'http://pogo.com'), 
-('Youtube', 'https://youtube.com'); 
-
-INSERT INTO tag_listings (listing_id, tag_id)
-VALUES
-(1,2),(1,3),(1,4),(1,5),(1,8),
-(2,2),(2,3),(2,4),(2,5),(2,8),
-(3,4),(3,8),
-(4,2),(4,3),(4,5),(4,8),
-(5,6),(5,8),
-(6,5),(6,6),(6,7),
-(7,6),(7,7),
-(8,8);
+('Youtube', 'https://youtube.com', 'Prepare to waste years of your life here.'); 
--- seeds/seed.primary-data.sql
+++ seeds/seed.primary-data.sql
@@ -12,16 +12,16 @@ VALUES
 ('Online', 8),
 ('Art', 9);
 
-INSERT INTO listings (name, url)
+INSERT INTO listings (name, url, description)
 VALUES
-('Official Documentation', 'http://reactjs.com'), 
-('W3Schools','http://w3schools.com'), 
-('Photoshop Tutorials', 'https://www.photoshopessentials.com/'), 
-('Javascript Official Site', 'https://javascript.com'), 
-('Addicting Games', 'http://addictinggames.com'), 
-('Board Game Geek', 'http://boardgamegeek.com'), 
-('Pogo Online Gaming', 'http://pogo.com'), 
-('Youtube', 'https://youtube.com'); 
+('Official Documentation', 'http://reactjs.com', 'A great site to learn all the official documentation on React. I highly recommend their walkthroughs'), 
+('W3Schools','http://w3schools.com', 'Okay site that really go the job done back in the day, but now theres a bit to be desired'), 
+('Photoshop Tutorials', 'https://www.photoshopessentials.com/', null), 
+('Javascript Official Site', 'https://javascript.com', 'Wanna learn javascript?'), 
+('Addicting Games', 'http://addictinggames.com', 'Prepare to waste hours of your life here.'), 
+('Board Game Geek', 'http://boardgamegeek.com', 'Board game ratings, reviews, community.'), 
+('Pogo Online Gaming', 'http://pogo.com', null), 
+('Youtube', 'https://youtube.com', 'Prepare to waste years of your life here.'); 
 
 INSERT INTO tag_listings (listing_id, tag_id)
 VALUES
