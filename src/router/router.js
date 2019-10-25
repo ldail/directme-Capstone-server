5d9a507e7d6470f4855d12e4df80e4a9251c3b27
Leon Dail <leondailofficial@gmail.com>
2019-10-24 23:25:02 -0700

    fixed the heroku error

--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = express.json();
 const routerService = require('./router-service');
-// const xss = require('xss');
+const xss = require('xss');
 
 router
 
@@ -10,42 +10,48 @@ router
     routerService.getAllTags(req.app.get('db'))
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
   .get('/hub_links',(req,res,next) => {
     routerService.getHubLinks(req.app.get('db'))
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
   .get('/hub_tags',(req,res,next) => {
     routerService.getHubTags(req.app.get('db'))
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
   .get('/listings',(req,res,next) => {
     routerService.getListings(req.app.get('db'))
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
 
   .get('/tagCount',(req,res,next) => {
     routerService.getTagCountsByPopularity(req.app.get('db'))
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
 
 
   .post('/tags/:name',(req,res,next) => {
     let {name} = req.params;
-    // let serialized = xss(name);
-    routerService.addTag(req.app.get('db'),name)
+    let serialized = xss(name);
+    routerService.addTag(req.app.get('db'),serialized)
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
 
   .post('/tag-listings',bodyParser,(req,res,next) => {
@@ -53,21 +59,23 @@ router
     routerService.addTagListing(req.app.get('db'),newTagListing)
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   })
 
   .post('/listings',bodyParser,(req,res,next) => {
     let listing = req.body;
-    // let serialized = {
-    //   name: xss(listing.name),
-    //   url: xss(listing.url),
-    //   description: xss(listing.description)
-    // };
+    let serialized = {
+      name: xss(listing.name),
+      url: xss(listing.url),
+      description: xss(listing.description)
+    };
 
-    routerService.addListing(req.app.get('db'),listing)
+    routerService.addListing(req.app.get('db'),serialized)
       .then(response => {
         return res.json(response);
-      });
+      })
+      .catch(next);
   });
 
 
