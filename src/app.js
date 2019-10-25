ab740787ce7eeff114c34dfaf8073f82f79792dd
Leon Dail <leondailofficial@gmail.com>
2019-10-24 23:12:15 -0700

    tried removing error handler

--- src/app.js
+++ src/app.js
@@ -21,12 +21,12 @@ app.use('/api', router);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === 'production') {
-    response = { error: {message: 'server error'} };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
+  // if (NODE_ENV === 'production') {
+  //   response = { error: {message: 'server error'} };
+  // } else {
+  //   console.error(error);
+  //   response = { message: error.message, error };
+  // }
   res.status(500).json(response);
 });
 
